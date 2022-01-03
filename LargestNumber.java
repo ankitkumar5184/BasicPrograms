8bfd69952d1d94354a0a15ee90231bdbab66e332
DELL <ankit.kumar@infostride.com>
2022-01-03 21:03:27 +0530

    Wrote program to find largest of 3 numbers using ternary operator

---
+++ LargestNumber.java
@@ -0,0 +1,10 @@
+public class LargestNumber
+{
+    public static void main(String[] args)
+    {
+        int a=195, b=190, c=155, d;
+
+        d = ( a > b ) ? (a > c ? a : c) : ( b > c ? b : c);
+        System.out.println("largest number "+d);
+    }
+}
