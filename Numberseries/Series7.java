4153c31e0ff42975c1b38a9daf2a086ebb7482da
DELL <ankit.kumar@infostride.com>
2022-01-04 19:20:44 +0530

    wrote a program on 1 12 123 1234 12345 1234 123 12 1

---
+++ Numberseries/Series7.java
@@ -0,0 +1,26 @@
+package Numberseries;
+
+public class Series7
+{
+    public static void main(String[] args)
+    {
+        int a = 6;
+        for(int i=1;i<=a;i++)
+        {
+            for(int j=1;j <=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        for(int i=a-1;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
