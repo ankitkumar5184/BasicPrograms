5ad7a29236b3cd373a86f4a7d2f21c8168f8bc4a
DELL <ankit.kumar@infostride.com>
2022-01-04 19:04:32 +0530

    wrote a program on 123456 12345 1234 123 12 1 series.

---
+++ Numberseries/Series5.java
@@ -0,0 +1,17 @@
+package Numberseries;
+
+public class Series5 {
+    public static void main(String[] args)
+    {
+        int a = 6;
+        for(int i=a;i>=1;i--)
+        {
+            for(int j=1;j <=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
